77fb224ac3a7d7c05298a425e45ab492cd0c3b79
VV4st3d <dfgc6935@gmail.com>
2023-08-23 03:09:44 +0300

    23 aug.

---
+++ src/pages/AboutPage/index.ts
@@ -0,0 +1,8 @@
+import {AboutPageAsync} from "./ui/AboutPageAsync";
+export {
+    AboutPageAsync as AboutPage,
+
+}
+
+
+//можно так: export {AboutPageAsync} from "pages/AboutPage/ui/AboutPageAsync";
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+
+interface NavbarProps {
+    className?: string,
+
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    // const arrayTheme:AppLinkTheme[] =
+    //     [AppLinkTheme.RED, AppLinkTheme.PRIMARY, AppLinkTheme.SECONDARY]
+    // const [appTheme, setAppTheme] = useState<AppLinkTheme>(AppLinkTheme.RED)
+    // function getRandomInt(max: number) {
+    //     return Math.floor(Math.random() * max);
+    // }
+    // function switcher(){
+    //     let randomTheme = arrayTheme[getRandomInt(3)]
+    //     while (randomTheme === appTheme){
+    //         console.log('repeat')
+    //         randomTheme = arrayTheme[getRandomInt(3)]
+    //     }
+    //     setAppTheme(randomTheme)
+    // }
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Main</AppLink>
+                <AppLink theme={AppLinkTheme.RED} to={'/about'}>About site</AppLink>
+                {/*<button onClick={switcher}>*/}
+                {/*    switch link theme*/}
+                {/*</button>*/}
+            </div>
+        </div>
+    );
+};
+
+
