64f364efffef0e147b235b366c857706f167f7ba
VV4st3d <dfgc6935@gmail.com>
2023-09-10 02:54:09 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,14 +1,13 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 
 
-export function buildPlugins({paths, isDev}: BuildOptions):webpack.WebpackPluginInstance[] {
-    return[
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+    const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html
         }),
@@ -20,10 +19,21 @@ export function buildPlugins({paths, isDev}: BuildOptions):webpack.WebpackPlugin
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin(),
         new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        }),
+
     ]
+    if (isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        )
+        plugins.push(
+            new BundleAnalyzerPlugin(
+                {
+                    openAnalyzer: false
+                }
+            )
+        )
+    }
+
+    return plugins
 }
