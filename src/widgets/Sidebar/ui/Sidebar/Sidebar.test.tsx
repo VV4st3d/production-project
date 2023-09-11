f064819dbb685b2bc8aa03faafd67ea8cfd20048
VV4st3d <dfgc6935@gmail.com>
2023-09-12 02:36:10 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,18 @@
 import {fireEvent, screen} from "@testing-library/react";
 import {Sidebar} from "widgets/Sidebar";
 import {withTranslation} from "react-i18next";
-import {renderWithTranslation} from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import {renderWithTranslation} from "shared/config/Tests/renderWithTranslation/renderWithTranslation";
+import {ComponentRender} from "shared/config/Tests/componentRender/componentRender";
 
 describe('Sidebar', function () {
     test('test sidebar',()=>{
-        renderWithTranslation(
+        ComponentRender(
             <Sidebar/>
         )
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
     test('test toggle',()=>{
-        renderWithTranslation(
+        ComponentRender(
             <Sidebar/>
         )
         const toggle = screen.getByTestId('sidebar-toggle')
