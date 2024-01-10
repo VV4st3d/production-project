4b4ee0522fe45ff2b634257b58e29ab87de3647d
VV4st3d <dfgc6935@gmail.com>
2024-01-10 18:50:49 +0300

    add alias config for jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -56,7 +56,8 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx')
+    '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx'),
+    '@/(.*)$': '<rootDir>/src/$1',
   },
   reporters: [
     'default',
