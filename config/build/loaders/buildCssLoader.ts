e131464197919f391a485863a84ff2d8f3612b8f
VV4st3d <dfgc6935@gmail.com>
2024-01-18 01:08:05 +0300

    upgrade webpack config, add babel cache

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -2,23 +2,24 @@ import {BuildOptions} from "../types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildCssLoader({isDev}: BuildOptions) {
-    return{
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                            localIdentName: isDev ? '[path][name]__[local][hash:base64:5]--' : '[hash:base64:8]'
-                        }
+    return {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local][hash:base64:5]--' : '[hash:base64:8]'
                     }
-                },
-                // Compiles Sass to CSS
-                "sass-loader",
-            ],
-        }
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
 }
