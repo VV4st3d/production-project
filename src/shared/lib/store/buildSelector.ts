ef09867e93b72383cc1028e13d52a2379b5dcfde
VV4st3d <dfgc6935@gmail.com>
2024-01-28 04:40:52 +0300

    add argument for selector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,15 @@
 import { StateScheme } from '@/app/providers/StoreProvider';
 import { useSelector } from 'react-redux';
 
-type Selector<T> = (state: StateScheme) => T;
-type Result<T> = [() => T, Selector<T>];
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => {
-        return useSelector(selector);
+type Selector<T, Args extends any[]> = (state: StateScheme, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
+
+export function buildSelector<T, Args extends any[]>(
+    selector: Selector<T, Args>,
+): Result<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+        return useSelector((state: StateScheme) => selector(state, ...args));
     };
 
     return [useSelectorHook, selector];
